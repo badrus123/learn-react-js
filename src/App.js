ad36fc3d3301e4d71a8a7eeab5815412b0b532d1
bbadrus123 <bbadrussholehaxel@gmail.com>
2019-04-04 21:57:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from "./Header"
+import Footer from "./Footer"
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header/>
+            <div class="card text-center">
+              <div class="card-header">
+                <ul class="nav nav-tabs card-header-tabs">
+                  <li class="nav-item">
+                    <a class="nav-link active" href="#">Active</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <input type="text" name="nama"></input><br></br>
+                <a href="#" class="btn btn-primary" name="input">Go somewhere</a>
+              </div>
+            </div>
+            <Footer/>
       </div>
     );
   }
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+
+const Footer = props => (
+    <div>
+            <div class="footer">
+                <p>Footer</p>
+            </div>
+    </div>
+  );
+export default Footer;
